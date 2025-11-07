d9eba55505ade4b5c011f0897e57ae65aabd8fa0
iagovic <161530831+iagovic@users.noreply.github.com>
2025-11-07 02:07:09 -0300

    Update iot1.sql

--- iot1.sql
+++ iot1.sql
@@ -2,17 +2,18 @@ CREATE TABLE motos_IOT (
     id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR2(100),
     status VARCHAR2(20),
-    contador NUMBER
+    contador NUMBER,
+    posicao NUMBER
 );
  
 -- Excluir a tabela (se já existir)
 DROP TABLE motos_IOT;
  
 -- Inserindo dados
-INSERT INTO motos_IOT (nome, status, contador) VALUES ('Moto A', 'parada', 0);
-INSERT INTO motos_IOT (nome, status, contador) VALUES ('Moto B', 'parada', 2);
-INSERT INTO motos_IOT (nome, status, contador) VALUES ('Moto C', 'parada', 5);
-INSERT INTO motos_IOT (nome, status, contador) VALUES ('Moto D', 'parada', 1);
+INSERT INTO motos_IOT (nome, status, contador, posicao) VALUES ('Moto A', 'parada', 0, 1);
+INSERT INTO motos_IOT (nome, status, contador, posicao) VALUES ('Moto B', 'parada', 2, 4 );
+INSERT INTO motos_IOT (nome, status, contador, posicao) VALUES ('Moto C', 'parada', 5, 3);
+INSERT INTO motos_IOT (nome, status, contador, posicao) VALUES ('Moto D', 'parada', 1, 2);
  
 -- Confirmar as alterações
 COMMIT;
